61ff8838c84dbb52414c0c4ee734c4b96a5b31f8
Hazim Okanovic <hazimokanovic@live.com>
2023-05-19 21:17:16 +0200

    Added locators and methods for reading errors on Create Account page

--- pages/createAccountPage.ts
+++ pages/createAccountPage.ts
@@ -3,15 +3,15 @@ import { Page } from "@playwright/test";
 export default class CreateAccountPage {
   constructor(public page: Page) { }
 
-  async enterFirstName(name: string) {
+  async enterFirstName( name: string ) {
     await this.firstNameInput.type(name);
   }
 
-  async enterLastName(name: string) {
+  async enterLastName( name: string ) {
     await this.lastNameInput.type(name);
   }
 
-  async enterEmail(email: string ) {
+  async enterEmail( email: string ) {
     await this.emailInput.type(email);
   }
 
@@ -19,12 +19,32 @@ export default class CreateAccountPage {
     await this.passwordInput.type(password);
   }
 
-  async enterConfirmPassword(password: string) {
+  async enterConfirmPassword( password: string ) {
     await this.passwordConfirmationInput.type(password);
   }
 
   async createAccountButtonClick() {
     await this.createAccountButton.click();
+  }
+
+  async getFirstNameError(){
+    await this.firstNameError.textContent();
+  }
+
+  async getLastNameError(){
+    await this.lastNameError.textContent();
+  }
+
+  async getEmailError(){
+    await this.emailError.textContent();
+  }
+
+  async getPasswordError(){
+    await this.passwordError.textContent();
+  }
+
+  async getPasswordConfirmationError(){
+    await this.passwordConfirmationError.textContent();
   }
 
   get firstNameInput(){
@@ -53,5 +73,25 @@ export default class CreateAccountPage {
 
   get pageTitle(){
     return this.page.locator("//span[@data-ui-id='page-title-wrapper']");
+  }
+
+  get firstNameError(){
+    return this.page.locator("#firstname-error");
+  }
+
+  get lastNameError(){
+    return this.page.locator("#lastname-error");
+  }
+
+  get emailError(){
+    return this.page.locator("#email_address-error");
+  }
+
+  get passwordError(){
+    return this.page.locator("#password-error");
+  }
+
+  get passwordConfirmationError(){
+    return this.page.locator("#password-confirmation-error");
   }
 }
